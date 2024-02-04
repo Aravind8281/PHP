77e245149c0244caa6679d7620e0de6758b0807c
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-02-04 12:14:49 +0530

    .

---
+++ PHP_Datatypes.php
@@ -0,0 +1,13 @@
+<?php
+$nums=10;
+$flonums=3.14;
+$string="Hello World";
+$arVar=array(1,2,3,4,5);
+$stVar=array(
+    "name"=>"aravind"
+);
+echo $nums ;
+echo $flonums ;
+echo $string;
+echo print_r($arVar);
+echo print_r($stVar);
